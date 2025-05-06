8099d962c5193db5ac790b30752fa404318ef351
MSP-2253\Thiago Bertoni <thiago.bertoni@monks.com>
2025-05-06 11:05:58 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import SiriEffect from './SiriEffect'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>Hello!</h1>
+      <SiriEffect />
+    </>
+  )
+}
+
+export default App
