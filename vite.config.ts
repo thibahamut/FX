76992c841d50daa2b7db2cf4772068306c74047d
MSP-2253\Thiago Bertoni <thiago.bertoni@monks.com>
2025-05-08 15:15:04 -0300

    Update base path in Vite configuration to '/FX/' for consistency with repository naming

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/fx/', // use o nome exato do seu repositório
+  base: '/FX/', // use o nome exato do seu repositório
   plugins: [react()],
 })
